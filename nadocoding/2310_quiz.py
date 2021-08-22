c8cb3d62612f4530a94239ede50369c537fd7a91
Song <w2033@e-mirim.hs.kr>
2021-08-22 22:05:44 +0900

    2310_quiz.py (5-1 ~5-6문제 풀이)Commit

--- nadocoding/2310_quiz.py
+++ nadocoding/2310_quiz.py
@@ -251,3 +251,24 @@ result = get_compliment("놀랄 만한 상황")
 print(result)
 result = get_compliment("좋은 마음가짐이다!")
 print(result)
+
+#퀴즈 5-1 ~ 5-5.
+    #1.모듈이란 - 비슷한 성실끼리의 함수나 변수 또는 클래스를 모아 놓은 파일이다.
+    #2.패키지란 - 모듈들을 모아둔 집합
+    #3.theater_module의 price 함수를 p학번 라는 이름으로 호출 하도록 import문을 작성
+from theater_module import price as p2310
+#p2310(2)
+    #4.__all__의 역할 - 실제 패키지를 만든 사람은 공개범위를 설정해 줄 수 있다.
+    # 이때 __all__이라는 변수에 리스트 형태로 공개하려는 모듈 이름을 추가하면 해당모듈에 대해 공개 설정을 할 수 있게 된다.
+    #5. if __name__ == "__main__":
+    #       trip_to = ThailandPackage()
+    #       trip_to.detail()
+    #   else:
+    #6. <가> trip_to = travel_temp.vietnam.VietnamPackage()
+    #        trip_to.detail()
+
+    #   <나> trip_to = vietnam.VietnamPackage()
+    #       trip_to.detail()
+
+    #   <다> trip_to = ThailandPackage()
+    #        trip_to.detail()
