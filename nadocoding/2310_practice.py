f32f78b05af73b2bf6194c1bd8b3a4f0def9b24a
Song <w2033@e-mirim.hs.kr>
2021-08-22 21:03:57 +0900

    2310_practice.py (module)Commit

--- nadocoding/2310_practice.py
+++ nadocoding/2310_practice.py
@@ -1013,3 +1013,26 @@ print("총 {0}대의 매물이 있습니다.".format(len(houses)))
 for house in houses:
     house.show_detail()
 
+#모듈
+# import theater_module
+# theater_module.price(3) #3명이서 영화 보러 갔을 때 가격
+# theater_module.price_morning(4) #4명이서 조조 할인 영화 보러 갔을 때
+# theater_module.price_soldier(5) #5명의 군인이 영화 보러 갔을 때
+
+# import theater_module as mv
+# mv.price(3)
+# mv.price_morning(4)
+# mv.price_soldier(5)
+
+# from theater_module import *
+# #from random import *
+# price(3)
+# price_morning(4)
+# price_soldier(5)
+
+# from theater_module import  price, price_morning
+# price(5)
+# price_morning(6)
+
+from theater_module import price_soldier as price_s
+price_s(5)
