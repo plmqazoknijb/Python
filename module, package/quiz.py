e924ecf49c56cb1d3fda3029fed5109ade9ec9c1
Song <w2033@e-mirim.hs.kr>
2021-09-07 12:17:04 +0900

    quiz(1~7 다른방법, 8번) Commit

--- module, package/quiz.py
+++ module, package/quiz.py
@@ -11,23 +11,35 @@ print(bill3)
 print('--2번 문제--')
 score = 56
 print(round(score,-1))
+#다른 방법
+print(round(score/10)*10)
 # 3. 로또 복권 자동 생성기를 만든다면?
 # (로또복권: 1~45 사이의 번호 중 랜덤으로 6개 뽑기)
 print('--3번 문제--')
 for a in range(6):
     lotto = random.randrange(1,45)
     print(lotto)
+#다른 방법
+random.sample(range(1,46),6)
 # 4. 1~9 숫자로 중복되지 않은 숫자 세자리를 뽑는 방법은?(185:O, 212:X)
 print('--4번 문제--')
 number = set()
 while len(number)<3:
     number.add(random.randint(1,9))
 print(number)
+#다른방법
+list_r = random.sample(range(1,10),3)
+print("".join(str(n)for n in list_r))
+print("".join(map(str,list_r)))
+# map()은 하나씩 꺼내서 적용한다.
 # 5. 내가 태어난 날로부터 며칠이 지났는지?
 print('--5번 문제--')
 now = datetime.datetime.now()
 birthday = datetime.datetime(2004,7,2,9,23)
+#엄마
+mbirthday = datetime.datetime(1970,5,30,0,0)
 print(now-birthday)
+print(now-mbirthday)
 
 # 6. 올해 크리스마스까지 며칠이 남았는지?
 print('--6번 문제--')
@@ -38,6 +50,29 @@ print(christmas-now)
 print('--7번 문제--')
 nextBirthday = datetime.datetime(2022,7,2,0,0)
 print(nextBirthday-now)
+# 다른 방법
+nextBirthday2 = datetime.datetime(2020,7,3,0,0)
+if nextBirthday2 < now:
+    nextBirthday2 = nextBirthday2.replace(year=nextBirthday2.year + 1)
+print(nextBirthday2 - now)
 
 # 8. 랜덤하게 번호로 자리를 배치하는 방법은?
 # 제적(전출, 자퇴) 인원이 있다면?
+# 마지막 번호가 몇 번인지 묻자
+last_number = input("마지막 번호는? ")
+#1부터 마지막 번호까지 리스트 만들자
+list_class = list(range(1,int(last_number)+1))
+
+# 무한 반복
+while True:
+# 나간 친구의 번호 묻자
+    exclude_number = input("뺄 번호는?(그냥 enter치면 끝내자)")
+# 그냥 enter면 반복 끝내자
+    if exclude_number =='':
+        break
+# 리스트에서 빼자
+    list_class.remove(int(exclude_number))
+    print(list_class)
+#랜덤으로 섞자
+#출력하자
+
