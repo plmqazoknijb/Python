84c7ebc851a2f558cc6ae0b43158a94fe83cf21d
Song <w2033@e-mirim.hs.kr>
2021-10-01 08:41:20 +0900

    Complete baseball.py(야구게임 예외 처리) Commit

--- module, package/baseball.py
+++ module, package/baseball.py
@@ -6,6 +6,15 @@ answer = make_quiz()
 while True:
 # 숫자 3자리 중북없이 묻자
     player = input("숫자 세자리는? ")
+    try:    #숫자가 아닐 때 에러 처리
+        palyer_int = int(player)
+    except ValueError:
+        continue
+    #길이가 3이 아닐 때 에러 처리
+    if len(player) != len(answer):
+        #raise InvalidCountError("3자리가 아닙니다.")
+        print(f'입력한 숫자의 개수가 정답과 다릅니다.정답: {len(answer)}')
+        continue
 # strike, ball 확인하자
     strike, ball = check(answer,player)
 # 출력하자
