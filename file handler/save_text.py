abfb0c2956820ab1c65e085e3a690bf2dccddee0
Song <w2033@e-mirim.hs.kr>
2021-10-05 11:13:58 +0900

    Complete save_text.py (with) Commit

--- file handler/save_text.py
+++ file handler/save_text.py
@@ -5,3 +5,9 @@ f.write('\n')
 f.write('월드')
 
 f.close()
+
+print('with')
+with open('text.txt', 'w', encoding='utf-8') as f:  #들여쓰기가 끝나는(with가 끝나는) 동시에 close가 자동으로 된다.
+    f.write('hello')
+    f.write('\n')
+    f.write('월드')
