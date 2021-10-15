b5933ee8b60d4854f57a227226ef8b06a529c25f
최민정 <s2019s37@e-mirim.hs.kr>
2021-10-15 10:10:26 +0900

    Edit baseball.py(top 3, save_history, load_history) Commit

--- module, package/baseball.py
+++ module, package/baseball.py
@@ -3,9 +3,32 @@ from baseball_game_engine import make_quiz, check
 answer = make_quiz()
 #print(answer)
 # 무한반복
+count = 0
+
+def save_history(player, count):
+    with open('baseball_history.txt','a') as f:
+        f.write(f'{player}\t{count}\n')
+
+
+def load_history():
+    count_list = []
+    with open('baseball_history.txt','r') as f:
+        while True:
+            line = f.readline()
+            if line == '':
+                break
+            # print(line.rstrip())
+            line_data = line.rstrip().split('\t')
+            count_list.append(line_data[1])
+    count_list.sort()
+    return count_list[:3]
 while True:
 # 숫자 3자리 중북없이 묻자
-    player = input("숫자 세자리는? ")
+    player = input("숫자 세자리는?(t: top3) ")
+    if player == 't':
+        history = load_history()
+        print(history)
+        continue
     try:    #숫자가 아닐 때 에러 처리
         palyer_int = int(player)
     except ValueError:
@@ -16,11 +39,14 @@ while True:
         print(f'입력한 숫자의 개수가 정답과 다릅니다.정답: {len(answer)}')
         continue
 # strike, ball 확인하자
+    count += 1
     strike, ball = check(answer,player)
 # 출력하자
-    print(f'{player}\tstrike: {strike}\tball: {ball}')
+    print(f'{player}\tstrike: {strike}\tball: {ball}\t{count}try')
 # strike가 3일때, 반복문 끝내기
-    if strike ==3:
+    if strike == 3:
+        #저장하자
+        save_history(player, count)
         break
 #축하해주자
 print('축하합니다. 짝짝짝👏👏')
